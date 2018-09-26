8dd2e2d10d1493c490819b7844cd2cbc86ca212e
mrfekry <m.ramadan@fcih.net #m.ramadan@fcih.netgit config --global user.email m.ramadan@fcih.net #m.ramadan@fcih.netgit config --global user.email m.ramadan@fcih.net>
2018-09-26 19:45:58 +0200

    adding unit tests

--- src/app/components/employee/employee-list/employee-list.component.spec.ts
+++ src/app/components/employee/employee-list/employee-list.component.spec.ts
@@ -2,6 +2,8 @@
 import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeListComponent } from './employee-list.component';
+import { EmployeeService } from '../../../services/employee/employee.service';
+import { from } from 'rxjs';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
@@ -20,5 +22,16 @@ describe('EmployeeListComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return array of employee', () => {
+    let arr = [1, 2, 3];
+
+    spyOn(<any>EmployeeService, 'getEmployees').and.returnValue(arr);
+
+    component.refresh();
+
+    expect(arr.length).toBeGreaterThan(0);
+    expect(arr).toEqual([1, 2, 3]);
   });
 });
